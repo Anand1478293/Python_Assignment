0e38a0ffa66be34b0805b7d8c409a5e38e56d118
Anand1478293 <Sharma.anand.8218@gmail.com>
2025-06-26 07:51:42 +0530

    Add files via upload

---
+++ Mathematical_Operation.py
@@ -0,0 +1,18 @@
+# Taking input from the user
+
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+
+# Mathematical Operation started
+
+# Addition of first and second number
+print('Addition:' ,a+b)
+
+# Subtraction of first and second number
+print('Addition:' ,a-b)
+
+# Multiplication of first and second number
+print('Addition:' ,a*b)
+
+# Division of first and second number
+print('Addition:' ,a/b)
