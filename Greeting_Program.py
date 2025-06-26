12b1a4a4c0a3532d08ec6625244972da3b8e602c
Anand1478293 <Sharma.anand.8218@gmail.com>
2025-06-26 07:50:12 +0530

    Add files via upload

---
+++ Greeting_Program.py
@@ -0,0 +1,8 @@
+# Taking user first and last name
+
+a1 = (input("Enter your first name: "))
+a2 = (input("Enter your Last name: "))
+
+# Creationg Personalized greeting for the user
+
+print("Hello,",a1+" "+a2," ! Welcome to the python program.")
